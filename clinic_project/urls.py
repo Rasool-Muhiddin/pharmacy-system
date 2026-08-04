387b459ff6e0b5f3c76582a28fc6b25894610d30
rasool tahseen <rasoolmuhiddin@gmail.com>
2026-08-05 00:32:03 +0300

    add sitemaps

--- clinic_project/urls.py
+++ clinic_project/urls.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from django.urls import path, include  # تأكد من إضافة include هنا
+from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+
 from pharmacy import views as pharmacy_views
+from pharmacy.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
- 
+
     path('pharmacy/', include('pharmacy.urls')),
 
     path('', pharmacy_views.landing_page, name='landing_page'),
+
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    ),
 ]
---
+++ pharmacy/sitemaps.py
@@ -0,0 +1,15 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class StaticViewSitemap(Sitemap):
+    priority = 1.0
+    changefreq = "weekly"
+
+    def items(self):
+        return [
+            "landing_page",
+        ]
+
+    def location(self, item):
+        return reverse(item)
