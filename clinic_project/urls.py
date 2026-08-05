a514ceb409eff13ded20c7bc6785a1001870eb6e
rasool tahseen <rasoolmuhiddin@gmail.com>
2026-08-05 17:26:10 +0300

    fix

--- clinic_project/urls.py
+++ clinic_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 
 from pharmacy import views as pharmacy_views
 from pharmacy.sitemaps import StaticViewSitemap
@@ -22,4 +23,13 @@ urlpatterns = [
         {'sitemaps': sitemaps},
         name='django.contrib.sitemaps.views.sitemap'
     ),
+    
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type="text/plain",
+        ),
+    ),
+
 ]
